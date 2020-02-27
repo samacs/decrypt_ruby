b9c86280f18d89e4326b13dfb8119b994fc1a2ea
Saul Martínez <saul.martinez05@gmail.com>
2020-02-27 14:29:11 -0600

    Put characters back.

--- waiting_id.php
+++ waiting_id.php
@@ -1,16 +1,16 @@
 <?php
-$examples = [['encrypt_client_key' => 'RffxeGOfiHjZ8oodYsHo6A==', 'ab' => 'brX3LevfNDOYMBtRl71HGw==', 'encrypted' => 'kjNuH2A=', 'waiting_id' => '64769'],
-             ['encrypt_client_key' => 'bnqk_t7et-PdXRPpaNfCAQ==', 'ab' => 'tPxEmIbZu0UL-eknZ8Zu6g==', 'encrypted' => '3CGQBy8=', 'waiting_id' => '60216'],
-             ['encrypt_client_key' => '4AYFWk2u0gYaioncZHCp_g==', 'ab' => 'wTbFvZROvPcjI71AhYbMdw==', 'encrypted' => 'kYiARKI=', 'waiting_id' => '62009'],
-             ['encrypt_client_key' => 'z4K7EbQrEtxZ5ocfYmI43Q==', 'ab' => 'vBdw8rlayLhf3YTlafKw8Q==', 'encrypted' => 'BFr8DBk=', 'waiting_id' => '69967'],
-             ['encrypt_client_key' => 'dkpr8EnwRdDe6L2NZceIWg==', 'ab' => 'vo_Vm85biXY6tBjfR-xIQA==', 'encrypted' => 'deiOfQg=', 'waiting_id' => '65474']];
+$examples = [['encrypt_client_key' => 'XOdMSzdcqnfIllYcdTwT-w==', 'ab' => 'tVpnRVbh1ZzZ-DBaeVdkdw==', 'encrypted' => 'uTgOZF0=', 'waiting_id' => '63304'],
+             ['encrypt_client_key' => 'SeB6hFbP2nO-L4lWcr1iIw==', 'ab' => 'idZ4IVQpMBFWZxiWPXjVFQ==', 'encrypted' => '41KH7Fs=', 'waiting_id' => '68111'],
+             ['encrypt_client_key' => 'JAVIRdd6S88APOuWkjnHiQ==', 'ab' => 'O3NfcJtpH71A828F0eXSXw==', 'encrypted' => 'U9XL0uc=', 'waiting_id' => '69677'],
+             ['encrypt_client_key' => 'wpvfnz_8MPuygoODRN1Mjg==', 'ab' => '-e6wo-kZgl6wFyZaLOTjEQ==', 'encrypted' => '8tWq9ec=', 'waiting_id' => '61640'],
+             ['encrypt_client_key' => 'ttoqsD0GSY2EseL7I16OEg==', 'ab' => '6biBxXK96dbRiZjrPBOZKw==', 'encrypted' => 'DQFdUmA=', 'waiting_id' => '66121']];
 
 $method = 'AES-128-CTR';
 foreach ($examples as $index => $example) {
     $no = $index + 1;
-    $key = base64_decode($example["encrypt_client_key"], true);
-    $iv = base64_decode($example["ab"], true);
-    $data = base64_decode($example["encrypted"], true);
+    $key = urlsafe_decode64($example["encrypt_client_key"]);
+    $iv = urlsafe_decode64($example["ab"]);
+    $data = urlsafe_decode64($example["encrypted"]);
     $result = openssl_decrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv);
     $passed = $result == $example["waiting_id"] ? 'Yes' : 'No';
     echo "[{$no}] Key: {$example["encrypt_client_key"]}\n";
@@ -18,4 +18,8 @@ foreach ($examples as $index => $example) {
     echo "[{$no}] Waiting ID: {$example["waiting_id"]}\n";
     echo "[{$no}] Result: {$result}\n";
     echo "[{$no}] Passed? {$passed}\n";
+}
+
+function urlsafe_decode64($data) {
+    return base64_decode(strtr($data, '-_', '+/'));
 }
